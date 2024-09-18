fc8c555b7c2a25d27ddfe5bc0dbfb5c3a959ec30
omarelemam49141 <114430558+omarelemam49141@users.noreply.github.com>
2024-09-18 22:07:42 +0300

    add ngx translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from './core/services/language.service';
+import  { sharedImports } from './shared/sharedImports';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, sharedImports],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  //properties
   title = 'embeded-store-front';
+
+  //injection
+  private translateService = inject(TranslateService);
+  private languageService = inject(LanguageService);
+
+  //life cycle
+  constructor() {
+    this.addSupportedLanguages();
+    this.setDefaultLanguage('en');
+    let currLang = this.languageService.getCurrentLanguage()
+    if (!currLang) {
+      this.useLanguageAccordingToBrowserPreference();
+    } else {
+      this.translateService.use(currLang);
+    }
+  }
+
+  //methods
+  changeLanguage(): void {
+    if(this.languageService.getCurrentLanguage() == 'en') {
+      this.languageService.changeCurrentLanguage('ar');
+    } else {
+      this.languageService.changeCurrentLanguage('en');
+    }
+  }
+  addSupportedLanguages(): void {
+    this.translateService.addLangs(['en', 'ar']);
+  }
+
+  setDefaultLanguage(lang: string): void {
+    this.translateService.setDefaultLang(lang);
+  }
+
+  useLanguageAccordingToBrowserPreference(): void {
+    const browserLang = this.translateService.getBrowserLang();
+    this.translateService.use(browserLang?.match(/en|ar/) ? browserLang : 'en');
+  }
 }
--- src/app/core/services/language.service.ts
+++ src/app/core/services/language.service.ts
@@ -1,17 +1,17 @@
 import { SessionStorageService } from './session-storage.service';
 import { inject, Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LanguageServiceService {
+export class LanguageService {
   //observables
   private isLangArabic = new Subject<boolean>;
 
   //injection
-  private sessionStorageService = inject(SessionStorageService);
+  private translateService = inject(TranslateService);
 
   //life cycle
   constructor() {
@@ -19,22 +19,30 @@ export class LanguageServiceService {
   }
 
   //methods
+  setBodyDirection(lang: string) {
+    document.body.dir = lang === 'ar' ? 'rtl' : 'ltr';
+  }
   setCurrentLanguageSubject(lang: string) {
     this.isLangArabic.next(lang === 'ar');
   }
   changeCurrentLanguage(lang: string): void {
     localStorage.setItem('lang', lang);
+    this.translateService.use(lang);
     this.setCurrentLanguageSubject(lang);
-    this.reloadCurrentRoute();
+    this.setBodyDirection(lang);
   }
   setLanguageInLocalStorage(): void {
-    let lang = this.sessionStorageService.lang;
+    let lang = localStorage.getItem('lang');
     if (!lang) {
+      lang = 'en';
       localStorage.setItem('lang', 'en');
+      this.translateService.use('en');
     } else {
-      localStorage.setItem('lang', 'ar');
+      localStorage.setItem('lang', lang);
+      this.translateService.use(lang);
     }
     this.setCurrentLanguageSubject(lang);
+    this.setBodyDirection(lang);
   }
   getIsLangArabic$(): Observable<boolean> {
     return this.isLangArabic.asObservable();
@@ -42,9 +50,5 @@ export class LanguageServiceService {
 
   getCurrentLanguage(): string {
     return localStorage.getItem('lang') || 'en';
-  }
-
-  reloadCurrentRoute(): void {
-    window.location.reload();
   }
 }
